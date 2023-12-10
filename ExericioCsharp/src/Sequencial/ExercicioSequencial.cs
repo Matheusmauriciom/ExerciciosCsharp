9629a0451649cedce25358d17275385939353333
Matheusmauriciom <150610041+Matheusmauriciom@users.noreply.github.com>
2023-12-10 14:44:05 -0300

    commit-criacao

---
+++ ExericioCsharp/src/Sequencial/ExercicioSequencial.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExericioCsharp.src.Sequencial
+{
+    public class ExercicioSequencial
+    {
+        
+    }
+}
