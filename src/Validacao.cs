1da469ef4d9dac33a613e5afedeced2bf76b11bf
Matheusmauriciom <150610041+Matheusmauriciom@users.noreply.github.com>
2023-12-25 10:07:41 -0300

    commit-attValidacao

--- src/Validacao.cs
+++ src/Validacao.cs
@@ -8,7 +8,8 @@ namespace ExericioCsharp.src
     public class Validacao
     {
         public static int ValidarNumero(string mensagem)
-        {   Console.Clear();
+        {
+            Console.Clear();
             int numero;
             bool entradaValida = false;
 
@@ -32,6 +33,37 @@ namespace ExericioCsharp.src
             //retorna o numero válido
             return numero;
         }
+
+        public static double ValidarNumeroDouble(string mensagem)
+        {
+            Console.Clear();
+            double numero;
+            bool entradaValida = false;
+
+            do
+            {
+                // Solicita ao usuário que insira um número
+                Console.Write(mensagem);
+
+                string input = Console.ReadLine();
+
+                // Tenta converter a entrada para um número de ponto flutuante (double)
+                if (double.TryParse(input, out numero))
+                {
+                    // Se a conversão for bem-sucedida, o loop encerra
+                    entradaValida = true;
+                }
+                else
+                {
+                    // Se a conversão falhar, exibe uma mensagem de erro
+                    Console.WriteLine("Por favor, digite um número válido");
+                }
+            }
+            while (!entradaValida);
+
+            // Retorna o número de ponto flutuante válido
+            return numero;
+        }
         public static string ValidarString(string mensagem)
         {
             string input;
@@ -43,7 +75,7 @@ namespace ExericioCsharp.src
 
                 //verifica se a entrada pode ser convertida para um num
                 if (int.TryParse(input, out _))
-                {   
+                {
                     //conversao for possivel = erro
                     Console.WriteLine("Porfavor, digite apenas texto, não números.");
                 }
@@ -51,9 +83,10 @@ namespace ExericioCsharp.src
 
             //retorn a string válida que foi inserada
             return input;
-        } 
+        }
         public static void AguardarTecla()
-        {   Console.WriteLine();
+        {
+            Console.WriteLine();
             Console.WriteLine("Precione qualquer tecla para finalizar...");
             Console.ReadKey();
             Console.Clear();
